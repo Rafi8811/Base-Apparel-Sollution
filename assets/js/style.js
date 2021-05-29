e6a4a05809afc3c497e722cbe9a2bfd5152c894f
Alif2001 <83873614+Rafi8811@users.noreply.github.com>
2021-05-29 15:08:21 +0600

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,6 +5,7 @@ function validation()
 	var email = document.getElementById("email").value;
 	var errormsg = document.getElementById("error-msg");
 	var erroricon = document.getElementById("error-icon");
+	var subbtn = document.getElementById("btn");
 	var pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 	if (email.match(pattern))
@@ -22,7 +23,9 @@ function validation()
       errormsg.innerHTML = "Please provide a valid email";
       errormsg.style.opacity = "1";
       erroricon.style.opacity = "1";
-      
+      subbtn.addEventListner("click" , function(event){
+	      event.preventDefault();
+      });
 	}
    
 
